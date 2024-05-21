3291f40cbccb850d09115986e1e3208137ac722a
Tushar Khokhar <148557736+itsme-boii@users.noreply.github.com>
2024-05-21 17:37:37 +0530

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -5,22 +5,27 @@ import {
   Transaction,
   TransactionInstruction,
   sendAndConfirmTransaction,
-  Account,
+  SystemProgram,
+  LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
 import { serialize } from "borsh";
 import { Buffer } from "buffer";
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-const progId = new PublicKey("Fay317m7Geo3eTzxFJd8EFK1tab8sciSKwDhEib2cArg");
-
+const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+const progId = new PublicKey("7MyWqb1JDgHGfkbuyGFvFaPQwLDKJyX2hymhj9NMSYuh");
+const authorityId = new PublicKey(
+  "G8XnWUNznBctaJ6iGz39S5ZkZCjeaSHCEtyvpJEaBokQ"
+);
 const wallet = Keypair.fromSecretKey(
   Uint8Array.from([
-    45, 134, 186, 18, 67, 205, 31, 12, 89, 188, 95, 14, 215, 2, 218, 188, 95,
-    222, 195, 124, 224, 246, 220, 58, 188, 174, 159, 80, 187, 148, 113, 26, 9,
-    36, 120, 57, 190, 48, 158, 152, 109, 243, 222, 194, 142, 254, 82, 208, 79,
-    58, 166, 180, 228, 192, 147, 120, 26, 113, 187, 131, 235, 214, 8, 21,
+    20, 242, 196, 8, 235, 129, 57, 129, 208, 151, 213, 166, 24, 31, 163, 9, 122,
+    142, 48, 254, 92, 61, 18, 240, 244, 36, 192, 244, 219, 157, 131, 76, 224,
+    205, 45, 135, 32, 39, 151, 130, 126, 140, 107, 149, 38, 34, 16, 109, 7, 125,
+    88, 26, 122, 237, 107, 184, 238, 0, 23, 59, 222, 24, 52, 205,
   ])
 );
+
+const ownerId = new PublicKey("CDns6TVDnPXsZTxGmMz2PjVK6kM156yuutReTGBsp3to");
 
 const gpuRegistryPubkey = new PublicKey(
   "cgvYffFMGJbj8Rvicsw6dDHT1dhMpSvE4DNvkSx9WLc"
@@ -47,66 +52,83 @@ const payloadSchema = new Map([
     },
   ],
 ]);
+const [storagePDA, _] = await PublicKey.findProgramAddressSync(
+  [Buffer.from("storagePool"), authorityId.toBuffer()],
+  progId
+);
 
 async function initialize_account(model: string): Promise<string> {
   const payload = new Payload(0, model);
   const serializedPayload = Buffer.from(serialize(payloadSchema, payload));
-  const accountPublicKey = new PublicKey(
-    "Dea7vK8jhKqNSV6EaHePiJ5GW4tUQbgutSrGBFuVTxwm"
-  );
-  const data = Buffer.from([0]);
-  const instruction = new TransactionInstruction({
-    data: serializedPayload,
-    keys: [{ pubkey: accountPublicKey, isSigner: false, isWritable: true }],
-    programId: progId,
-  });
-
-  const transactionSignature = await sendAndConfirmTransaction(
-    connection,
-    new Transaction().add(instruction),
-    [wallet],
-    {
-      commitment: "confirmed",
-      preflightCommitment: "confirmed",
-    }
-  );
-  console.log("Transaction Signature =", transactionSignature);
-  return transactionSignature;
-}
-
-async function registerGPU(model: string): Promise<string> {
-  //  payload for RegisterGpu
-  const payload = new Payload(1, model); // Variant 1 corresponds to RegisterGpu
-  console.log("Payload:", payload);
-
-  // payload
-  const serializedPayload = Buffer.from(serialize(payloadSchema, payload));
-
+  const storageAccount = Keypair.generate();
   const instruction = new TransactionInstruction({
     data: serializedPayload,
     keys: [
-      { pubkey: gpuRegistryPubkey, isSigner: false, isWritable: true },
       { pubkey: wallet.publicKey, isSigner: true, isWritable: false },
+      { pubkey: storagePDA, isSigner: false, isWritable: true },
+      { pubkey: SystemProgram.programId, isSigner: false, isWritable: true },
     ],
     programId: progId,
   });
 
-  // Send Solana Transaction
-  const transactionSignature = await sendAndConfirmTransaction(
-    connection,
-    new Transaction().add(instruction),
-    [wallet],
-    {
-      commitment: "confirmed",
-      preflightCommitment: "confirmed",
-    }
-  );
-  console.log("Transaction Signature =", transactionSignature);
-  return transactionSignature;
+  try {
+    const transactionSignature = await sendAndConfirmTransaction(
+      connection,
+      new Transaction().add(instruction),
+      [wallet],
+      {
+        commitment: "confirmed",
+        preflightCommitment: "confirmed",
+      }
+    );
+    console.log("Transaction Signature =", transactionSignature);
+    return transactionSignature;
+  } catch (error) {
+    console.error("Error registering GPU:", error);
+    throw error;
+  }
 }
 
-const gpuModel = "Rtx3030";
-initialize_account(gpuModel);
-registerGPU(gpuModel)
+async function create_entry(model: string): Promise<string> {
+  const payload = new Payload(1, model);
+  const serializedPayload = Buffer.from(serialize(payloadSchema, payload));
+  const [derivedPDA, _] = await PublicKey.findProgramAddressSync(
+    [Buffer.from("Entry"), Buffer.from(model), authorityId.toBuffer()],
+    progId
+  );
+  const storageAccount = Keypair.generate();
+  // const serializedPayload = Buffer.from(serialize(payloadSchema, payload));
+  const instruction = new TransactionInstruction({
+    data: serializedPayload,
+    keys: [
+      { pubkey: wallet.publicKey, isSigner: true, isWritable: false },
+      { pubkey: storagePDA, isSigner: false, isWritable: true },
+      { pubkey: derivedPDA, isSigner: false, isWritable: true },
+      { pubkey: SystemProgram.programId, isSigner: false, isWritable: true },
+    ],
+    programId: progId,
+  });
+
+  try {
+    const transactionSignature = await sendAndConfirmTransaction(
+      connection,
+      new Transaction().add(instruction),
+      [wallet],
+      {
+        commitment: "confirmed",
+        preflightCommitment: "confirmed",
+      }
+    );
+    console.log("Transaction Signature =", transactionSignature);
+    return transactionSignature;
+  } catch (error) {
+    console.error("Error registering GPU:", error);
+    throw error; // Rethrow the error for proper handling in the calling function.
+  }
+}
+
+const gpuModel = "yuhh";
+create_entry(gpuModel)
+  // registerGPU(gpuModel)
   .then(() => console.log("GPU registration complete"))
   .catch((error) => console.error("Error registering GPU:", error));
